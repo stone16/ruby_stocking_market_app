b67348763d3846aaa73ff7ff57b2247195391263
stone16 <stone2paul@gmail.com>
2022-01-14 14:24:15 +0800

    Add error handler

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,14 @@ class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_f96573054f054ea89d898452cbf16cb2')
 
-    if params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
+    if params[:ticker] == ''
+      @nothing = 'Please enter some symbol for searching. '
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue StandardError => e
+        @error = 'Wrong ticker, please check your input'
+      end
     end
   end
 

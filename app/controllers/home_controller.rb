3c4f97f4db3ec3c86ce95624610d35bd4a5ae029
stone16 <stone2paul@gmail.com>
2022-01-14 13:59:57 +0800

    support form and get stock company name by searching with ticker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_f96573054f054ea89d898452cbf16cb2')
+
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+    end
   end
 
   def about

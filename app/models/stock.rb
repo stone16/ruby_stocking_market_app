0f0efb0b96e1bccb214b85532a40d86d4c01807b
stone16 <stone2paul@gmail.com>
2022-01-17 15:31:44 +0800

    A workable version of stock app

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 # Stock model
 class Stock < ApplicationRecord
-  validate :ticker, presence: true
-  validate :ticker, format: {without: /\s/}
+  validates :ticker, presence: true
+  validates :ticker, format: {without: /\s/}
   belongs_to :user
 end
